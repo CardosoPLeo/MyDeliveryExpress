1c5c8dd3ffd4976bebcf3e47bd1627ace411c291
CardosoPLeo <leonardo_p_c@hotmail.com>
2021-10-18 22:21:13 -0300

    Implementado a classe produto

--- src/main/java/model/Produto.java
+++ src/main/java/model/Produto.java
@@ -1,4 +1,61 @@
 package model;
 
 public class Produto {
+    private Integer idProduto;
+    private String nome;
+    private Double precoUnitario;
+    private Integer qtdProduto;
+    private String descricao;
+
+    public Produto() {
+        super();
+    }
+
+    public Produto(Integer idProduto, String nome, Double precoUnitario, Integer qtdProduto, String descricao) {
+        this.idProduto = idProduto;
+        this.nome = nome;
+        this.precoUnitario = precoUnitario;
+        this.qtdProduto = qtdProduto;
+        this.descricao = descricao;
+    }
+
+    public Integer getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(Integer idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(Double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public Integer getQtdProduto() {
+        return qtdProduto;
+    }
+
+    public void setQtdProduto(Integer qtdProduto) {
+        this.qtdProduto = qtdProduto;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
