ae92362d3b1de96794a31b78e097c351664e969d
CardosoPLeo <leonardo_p_c@hotmail.com>
2021-10-18 22:05:11 -0300

    Implementado a classe funcionario

--- src/main/java/model/Funcionario.java
+++ src/main/java/model/Funcionario.java
@@ -1,4 +1,42 @@
 package model;
 
 public class Funcionario {
+    private Integer idFuncionario;
+    private String nome;
+    private String telefone;
+
+    public Funcionario() {
+        super();
+    }
+
+    public Funcionario(Integer idFuncionario, String nome, String telefone) {
+        this.idFuncionario = idFuncionario;
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public Integer getIdFuncionario() {
+        return idFuncionario;
+    }
+
+    public void setIdFuncionario(Integer idFuncionario) {
+        this.idFuncionario = idFuncionario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
 }
