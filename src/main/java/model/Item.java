4fbd43d8b0b1e76477e81e761c25ba9478a3a051
CardosoPLeo <leonardo_p_c@hotmail.com>
2021-10-18 23:03:13 -0300

    Implementado a classe item

--- src/main/java/model/Item.java
+++ src/main/java/model/Item.java
@@ -1,4 +1,35 @@
 package model;
 
-public class Item {
+public class Item extends Produto{
+    private Integer idItem;
+    private Integer quantidade;
+    public Item(String nome, Double precoUnitario) {
+        super(nome, precoUnitario);
+    }
+
+    public Item() {
+        super();
+    }
+
+    public Item(String nome, Double precoUnitario, Integer idItem, Integer quantidade) {
+        super(nome, precoUnitario);
+        this.idItem = idItem;
+        this.quantidade = quantidade;
+    }
+
+    public Integer getIdItem() {
+        return idItem;
+    }
+
+    public void setIdItem(Integer idItem) {
+        this.idItem = idItem;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
 }
