72e76c958444bf90cac6d1085173786b5a0b3f8a
CardosoPLeo <leonardo_p_c@hotmail.com>
2021-10-18 22:01:34 -0300

    Implementação da classe de cliente

--- src/main/java/model/Cliente.java
+++ src/main/java/model/Cliente.java
@@ -1,4 +1,51 @@
 package model;
 
 public class Cliente {
+    private Integer idCliente;
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    public Cliente() {
+        super();
+    }
+
+    public Cliente(Integer idCliente, String nome, String endereco, String telefone) {
+        this.idCliente = idCliente;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public Integer getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
